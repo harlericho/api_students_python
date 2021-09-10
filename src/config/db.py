381352c2fd8103a65325d65aacbaebb44bc0051e
charlie <harlericho@gmail.com>
2021-09-10 17:20:58 -0500

    configuracion terminada

--- src/config/db.py
+++ src/config/db.py
@@ -2,6 +2,6 @@
 def get_connection(app):
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'charlie'
-    app.config['MYSQL_PASSWORD'] = 'charlie86'
+    app.config['MYSQL_PASSWORD'] = 'charlie'
     app.config['MYSQL_DB'] = 'db_api_students'
     return app
